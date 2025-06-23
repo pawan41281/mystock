35b81e6f5d28149f98d6718af64240ea8fe37147
Pawan Kumar <pawan41281@gmail.com>
2025-06-23 08:58:50 +0530

    initial commit

---
+++ src/main/java/org/mystock/MystockApplication.java
@@ -0,0 +1,13 @@
+package org.mystock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MystockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MystockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mystock/MystockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mystock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MystockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
